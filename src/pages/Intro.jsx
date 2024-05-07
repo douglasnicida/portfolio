4170e15f110925178b155883af421e6151f9a037
Douglas Nicida <douglasnicida@gmail.com>
2024-05-07 00:48:09 -0300

    feat: Sequence Random Reveal Words for intro section

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,19 +1,44 @@
+import { useScramble } from "use-scramble";
+
 import {ReactComponent as Github} from "../assets/github.svg"
 import {ReactComponent as Linkedin} from "../assets/linkedin.svg"
 import {ReactComponent as Mouse} from "../assets/mouse.svg"
+import { useState } from "react";
 
 const Intro = () => {
+    const [alternate, setAlternate] = useState(false)
+
+    // RANDOM REVEAL JAPANESE CHARACTERS ANIMATION
+    const JapaneseRandomReveal = ({text, className, replayActive}) => {
+        const { ref, replay } = useScramble({ 
+            text: text,
+            range: [12448, 12543.0],
+            speed: 0.5,
+            tick: 2,
+            seed: 5,
+            onAnimationEnd: () => {setTimeout(()=>{setAlternate(!alternate)}, 5000)}
+        });
+
+        let res;
+
+        (replayActive) ? res = <p ref={ref} onMouseOver={replay} className={className} /> : res = <p ref={ref} className={className}/>
+      
+        return res
+    };
+
     return ( 
         <div className="flex flex-col relative h-[calc(100vh)] p-responsiveLayout md:p-layout justify-center items-center text-backgroundDark bg-backgroundLight dark:text-backgroundLight dark:bg-backgroundDark z-10" id="intro">
             
             {/* MAIN CONTENT */}
             <div className="flex flex-col relative xl:-mt-12">
-                <h2 className="text-myRed text-md lg:text-xl uppercase tracking-widest">Douglas Nicida</h2>
+                
+                <JapaneseRandomReveal text={"Douglas Nicida"} className={"text-myRed text-md lg:text-2xl uppercase tracking-widest w-fit"} replayActive={true}/>
                 
                 <div className="flex flex-col relative mt-5">
                     {/* UPPER */}
                     <div className="flex h-auto mb-7 md:mb-9 lg:mb-16">
-                        <h1 className="uppercase text-5xl sm:text-6xl lg:text-8xl font-bold">Dev Web</h1>
+                        <JapaneseRandomReveal text={"Dev Web"} className={"uppercase text-5xl sm:text-6xl lg:text-8xl font-bold"} replayActive={false}/>
+                        
                         <div className="rotate-[90deg] ml-[44px] lg:ml-11 md:-mt-3 lg:mt-0">
                             <div className="bg-introDetails h-[7px] w-[40px] md:w-[60px] lg:w-[80px]"></div>
                             <div className="bg-introDetails h-[80px] md:h-[120px] lg:h-[140px] w-[7px] lg:w-[7px]"></div>
@@ -26,7 +51,12 @@ const Intro = () => {
                             <div className="bg-introDetails h-[7px] lg:h-[7px] w-[40px] md:w-[60px] lg:w-[80px]"></div>
                             <div className="bg-introDetails h-[80px] md:h-[120px] lg:h-[140px] w-[7px] lg:w-[7px]"></div>
                         </div>
-                        <h1 className="uppercase text-5xl sm:text-6xl lg:text-8xl font-bold ml-10">Fullstack</h1>
+
+                        {(alternate === false) ? 
+                            <JapaneseRandomReveal text={"FULLSTACK"} className={"uppercase text-5xl sm:text-6xl lg:text-8xl font-bold ml-10 w-fit"} replayActive={false}/>
+                            :
+                            <JapaneseRandomReveal text={"DESIGNER"} className={"uppercase text-5xl sm:text-6xl lg:text-8xl font-bold ml-10 w-fit"} replayActive={false}/>
+                        }
                     </div>
                 </div>
             </div>
