d97f28b92973f349e011b62a99a51cb8cdad8959
Douglas Nicida <douglasnicida@gmail.com>
2024-05-08 02:30:49 -0300

    feat: initial Services page structure

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,6 @@
 const Services = () => {
     return ( 
-        <div className="flex flex-col relative h-[100vh] sm:p-responsiveLayout md:p-layout justify-center items-center text-backgroundDark bg-backgroundLight dark:text-backgroundLight dark:bg-backgroundDark z-10" id="skills">
+        <div className="flex flex-col relative h-[100vh] sm:p-responsiveLayout md:p-layout justify-center items-center text-backgroundDark bg-backgroundLight dark:text-backgroundLight dark:bg-backgroundDark z-10" id="services">
             
         </div>
      );
