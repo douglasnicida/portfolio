90f70210047e7cad71150135b2d43a860eee2a1b
Douglas Nicida <56737480+douglasnicida@users.noreply.github.com>
2024-05-10 01:27:58 -0300

    env file deleted

--- public/env.js
+++
@@ -1,6 +0,0 @@
-window.env = {
-  "ABSTRACT_EMAIL_API_KEY": "7a1341ede3604811a73d9016726d49bd",
-  "EMAILJS_SERVICE_ID": "service_84p5jje",
-  "EMAILJS_TEMPLATE_ID": "template_2yu81hj",
-  "EMAILJS_PUBLIC_API_KEY": "OxXUQ_L1r1y_hbjnj"
-};
